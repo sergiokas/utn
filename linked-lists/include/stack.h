7735929f9cf0aeecc23b501098ddae74b34bb17b
sergiokas <sergiokas@gmail.com>
2011-08-12 17:01:49 -0300

    Adding some comments

--- linked-lists/include/stack.h
+++ linked-lists/include/stack.h
@@ -13,7 +13,10 @@ struct stack_node {
 	struct stack_node * next;
 };
 
+// Push a data item to the stack
 void stack_push (struct stack_node **, void *);
+
+// Pop a data item from the stack, return a pointer to it.
 void * stack_pop (struct stack_node **);
 
 #endif /* STACK_H_ */
